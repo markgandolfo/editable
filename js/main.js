fee6d7328f010a480901305dd246bbeccca31b99
Mark Gandolfo <mark@aussiev8.com.au>
2013-03-06 20:24:08 +1000

    tabs now kind of works

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ window.onload = function() {
   var filename = document.getElementById("filename");
   var myTab = document.getElementById('myTab');
   var saved = false;
-  current_active_tab = 'new';
+  current_active_tab = 'scratch';
 
   tabs = {
-    'new': {
+    'scratch': {
       'content':'Start typing your note in here. It\'ll be stored to your local-storage on your browser. Simple as that!',
       'edited':false
     }
@@ -20,37 +20,22 @@ window.onload = function() {
   }
   content.style.height= window.innerHeight - 100;
 
-  content.addEventListener("onkeypress", function() {
-    saved = false;
+  load_open_options();
+  add_tab('scratch', {'close': false});
+
+  content.innerHTML = localStorage.getItem('scratch_editable') == null ? tabs['scratch']['content'] : localStorage.getItem('scratch_editable');
+
+  content.addEventListener('input', function() {
+    name = this.getAttribute('data-name');
+    console.log(name);
+    tabs[name]['content'] = $(this).html();
+    console.log(name);
+    save_tab(name);
+    console.log('saving');
   });
-
-  for (var key in localStorage){
-    if(key.indexOf("_editables") > 0) {
-      var li=document.createElement("li");
-      var a=document.createElement("a");
-
-      var name = key.substring(0,key.indexOf("_editables"));
-      a.setAttribute('data-name', name);
-      a.setAttribute('href', '#');
-      a.innerHTML = name;
-      li.appendChild(a);
-      saved_editables.appendChild(li);
-
-      a.addEventListener('click', function(e) {
-        e.preventDefault();
-        load_editable(this.getAttribute("data-name"));
-      });
-    }
-  }
-
-  $('#myTab a.tab').click(function (e) {
-    e.preventDefault();
-    switch_tab(this);
-  });
-  content.innerHTML = tabs['new']['content'];
 
   document.getElementById("save").addEventListener("click", function () {
-    if(current_active_tab == 'new') {
+    if(current_active_tab == 'scratch') {
       $('#nameModal').modal('show');
     } else {
       localStorage.setItem(current_active_tab.value + "_editables", content.innerHTML);
@@ -61,36 +46,60 @@ window.onload = function() {
 
   document.getElementById("new").addEventListener("click", function() {
     if(saved || content.innerHTML != "" && confirm("You'll loose any changes you've made that are unsaved in the new tab! Are you sure?")) {
-      switch_tab('new');
+      switch_tab('scratch');
       content.innerHTML = "";
-      tabs['new']['content'] = '';
+      tabs['scratch']['content'] = '';
     }
   });
-
-  // document.getElementById('.tab').addEventListener('ondblclick', function() {
-  //   console.log('double click bitches');
-  // });
 
   document.getElementById('save_modal_button').addEventListener('click', function() {
     value = document.getElementById('modal_filename').value;
     if(value != "") {
       current_active_tab = value;
-      //set_title();
-      //$('#nameModal').modal('hide');
       localStorage.setItem(value + "_editables", content.innerHTML);
       say("Success", value + " saved successfully", "success");
+    }
+  });
+
+  document.getElementById('save_rename_button').addEventListener('click', function() {
+    value = document.getElementById('modal_name').value;
+    if(value != "") {
+      tabs[value] = {
+        'content': tabs[renaming]['content'],
+        'edited': false
+      };
+      delete tabs[renaming];
+      $('li[data-name='+renaming+'] a.tab').html(value);
+      $('li[data-name='+renaming+'] a').attr('data-name', value);
+      $('li[data-name='+renaming+']').attr('data-name', value);
+      $('#saved_editables li a[data-name='+renaming+']').html(value);
+      $('#saved_editables li a[data-name='+renaming+']').attr('data-name', value);
+
+      localStorage.setItem(value + "_editables", content.innerHTML);
+      localStorage.removeItem(renaming + "_editables");
+      $('#renameModal').modal('hide');
     }
   });
 }
 
   function load_editable(name) {
-    add_tab(name);
+    add_tab(name, {'close': true });
     switch_tab(name);
   }
 
-  function add_tab(name) {
-    tabs[name] = {'content':localStorage.getItem(name + "_editables"), 'edited':false};
+  function add_tab(name, options) {
+    if($('.tabs li[data-name='+name+']').length > 0) {
+      return false;
+    }
 
+    var add_close_link = options['close'] == true ? true : false;
+    var tab_content = options['content'] !== undefined ? options['content'] : localStorage.getItem(name + "_editables");
+
+    tabs[name] = {
+      'content': tab_content,
+      'edited': false
+    }
+    
     var li=document.createElement("li");
     li.className = "active";
     li.setAttribute('data-name',name);
@@ -102,17 +111,24 @@ window.onload = function() {
     a.innerHTML = name;
     li.appendChild(a);
 
-    var close_link = document.createElement('a');
-    close_link.setAttribute('data-name', name);
-    close_link.setAttribute('data-action','close');
-    close_link.setAttribute('href', '#');
-    close_link.innerHTML = " x"
-    close_link.addEventListener('click', function() {
-      close_tab_and_save(name);
-    });
-    li.appendChild(close_link);
+    if(add_close_link) {
+      var close_link = document.createElement('a');
+      close_link.setAttribute('data-name', name);
+      close_link.setAttribute('data-action','close');
+      close_link.setAttribute('href', '#');
+      close_link.innerHTML = " x"
+      close_link.addEventListener('click', function() {
+        close_tab_and_save(this.getAttribute('data-name'));
+      });
+      li.appendChild(close_link);      
+    }
 
     myTab.appendChild(li);
+
+    a.addEventListener('dblclick', function() {
+      renaming = a.getAttribute('data-name');
+      $('#renameModal').modal('show');
+    });
 
     a.addEventListener('click', function(e) {
       e.preventDefault();
@@ -122,7 +138,7 @@ window.onload = function() {
 
   function close_tab_and_save(name) {
     save_tab(name);
-    switch_tab('new');
+    switch_tab('scratch');
     close_tab(name);
     delete tabs[name];
   }
@@ -139,7 +155,7 @@ window.onload = function() {
   }
 
   function save_tab(name) {
-    localStorage.setItem(name, tabs[name]['content']);
+    localStorage.setItem(name  + '_editables', tabs[name]['content']);
   }
 
   function switch_tab(name) {
@@ -183,3 +199,23 @@ window.onload = function() {
     },5000);
   }
 
+  function load_open_options() {
+    for (var key in localStorage){
+      if(key.indexOf("_editables") > 0) {
+        var li=document.createElement("li");
+        var a=document.createElement("a");
+
+        var name = key.substring(0,key.indexOf("_editables"));
+        a.setAttribute('data-name', name);
+        a.setAttribute('href', '#');
+        a.innerHTML = name;
+        li.appendChild(a);
+        saved_editables.appendChild(li);
+
+        a.addEventListener('click', function(e) {
+          e.preventDefault();
+          load_editable(this.getAttribute("data-name"));
+        });
+      }
+    }
+  }
