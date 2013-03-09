9836c1d6880908133098e1e41d7b70a64cac6800
Mark Gandolfo <mark@aussiev8.com.au>
2013-03-09 18:31:17 +1000

    grouped the add new tab modal together

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 window.onload = function() {
-
-  var form = document.getElementById("form_editables");
   var saved_editables = document.getElementById("saved_editables");
-  content = document.getElementById("content");
-  var filename = document.getElementById("filename");
-  var myTab = document.getElementById('myTab');
-  var saved = false;
-  current_active_tab = 'scratch';
+  var content = document.getElementById("content");
+  var current_active_tab = 'scratch';
 
   tabs = {
     'scratch': {
@@ -15,36 +10,50 @@ window.onload = function() {
     }
   };
 
-  unnamed = 1;
+  /* 
+    When the page loads, lets set up the page and scaffold ContentEditable
+  */
 
+  //  Set up the content editable height, and set an event to resize on window resize
+  content.style.height= window.innerHeight - 100;
   window.onresize = function(event) { 
     content.style.height= window.innerHeight - 100;
   }
-  content.style.height= window.innerHeight - 100;
-
+  
+  // Load all the open options and set up the initial scratch pad(tab
   load_open_options();
-  add_tab('scratch', {'close': false});
 
+  // Load the scratch pad if it exists, or else default to the default message from the "tabs" object above
+  add_tab('scratch', {'close': false});
   content.innerHTML = localStorage.getItem('scratch_editable') == null ? tabs['scratch']['content'] : localStorage.getItem('scratch_editable');
+  
+  // Focus the cursor on the content editable area
   content.focus();
 
+  // Event for listening for any type of input on the page, this will only work in very new browsers.. 
   content.addEventListener('input', function() {
     name = this.getAttribute('data-name');
     tabs[name]['content'] = $(this).html();
     save_tab(name);
   });
 
+  /*
+    Add new Tab 
+   */
+
+  // Event Handler for the new tab "button"
   document.querySelectorAll('li[data-action=new]')[0].addEventListener('click', function() {
     $('#newModal').modal('show');
-    $('#newModal').on('shown', function() {
-      $('#save_new_name').focus();
-    });
+    $('#newModal').on(
+      'shown', function() {
+        $('#save_new_name').focus();
+      },
+      'hidden', function() {
+        content.focus();
+      });
   });
 
-  $('#newModal').on('hidden', function() {
-    content.focus();
-  });
-
+  // for the New Tab Modal, listen for an enter keypress on the input field, catch it and simuilate the "save" button
   $(save_new_name).on('keyup', function(e){
     if(( e.keyCode == 13 ) && ( document.getElementById('save_new_name').value != "" )){
       create_new_tab_handler();
@@ -52,10 +61,12 @@ window.onload = function() {
     }
   });
 
+  // Save button on the New Tab Modal
   document.getElementById('save_new_button').addEventListener("click", function() {
     create_new_tab_handler()
   });
 
+  // Function for the submission of the New Tab Modal
   function create_new_tab_handler() {
     var save_new_name = document.getElementById('save_new_name');
 
@@ -74,22 +85,9 @@ window.onload = function() {
     }
   }
 
-  function editable_exists(name) {
-    if(localStorage.getItem(name + '_editables').length > 0) {
-      return true;
-    }
-    return false;
-  }
-
-  document.getElementById('save_modal_button').addEventListener('click', function() {
-    value = document.getElementById('modal_filename').value;
-    if(value != "") {
-      current_active_tab = value;
-      localStorage.setItem(value + "_editables", content.innerHTML);
-      say("Success", value + " saved successfully", "success");
-    }
-  });
-
+  /*
+    Rename a Tab 
+   */
   document.getElementById('save_rename_button').addEventListener('click', function() {
     value = document.getElementById('modal_name').value;
     if(value != "") {
@@ -109,6 +107,15 @@ window.onload = function() {
       $('#renameModal').modal('hide');
     }
   });
+
+
+  function editable_exists(name) {
+    if(localStorage.getItem(name + '_editables').length > 0) {
+      return true;
+    }
+    return false;
+  }
+
 }
 
   function load_editable(name) {
@@ -116,53 +123,6 @@ window.onload = function() {
     switch_tab(name);
   }
 
-  function add_tab(name, options) {
-    if($('.tabs li[data-name='+name+']').length > 0) {
-      return false;
-    }
-
-    var add_close_link = options['close'] == true ? true : false;
-    var tab_content = options['content'] !== undefined ? options['content'] : localStorage.getItem(name + "_editables");
-
-    tabs[name] = {
-      'content': tab_content,
-      'edited': false
-    }
-    
-    var li=document.createElement("li");
-    li.className = "active";
-    li.setAttribute('data-name',name);
-
-    var a=document.createElement("a");      
-    a.setAttribute('data-name', name);
-    a.setAttribute('href', '#');
-    a.className = 'tab';
-    a.innerHTML = name;
-    li.appendChild(a);
-
-    if(add_close_link) {
-      var close_link = document.createElement('a');
-      close_link.setAttribute('data-name', name);
-      close_link.setAttribute('data-action','close');
-      close_link.setAttribute('href', '#');
-      close_link.innerHTML = " x"
-      close_link.addEventListener('click', function() {
-        close_tab_and_save(this.getAttribute('data-name'));
-      });
-      li.appendChild(close_link);      
-    }
-
-    $('#myTab li[data-action=new]').before(li);
-    a.addEventListener('dblclick', function() {
-      renaming = a.getAttribute('data-name');
-      $('#renameModal').modal('show');
-    });
-
-    a.addEventListener('click', function(e) {
-      e.preventDefault();
-      switch_tab(a);
-    });
-  }
 
   function close_tab_and_save(name) {
     save_tab(name);
@@ -208,25 +168,6 @@ window.onload = function() {
     document.querySelector('li[data-name='+name+']').className = 'active';
   }
 
-  function set_title() {
-    if(filename.value == "") {
-      document.title = "Content Editable";
-    } else {
-      document.title = filename.value + " - Content Editable";
-    }
-  }
-
-  function say(title,message,color) {
-    var e = document.createElement('div');
-    e.innerHTML = "<h4>"+title+"</h4>" + message;
-    e.className = color == "success" ? "alert alert-success" : "alert alert-error";
-    document.getElementById("navbar").appendChild(e);
-    setTimeout( function(){
-      e.innerHTML = "";
-      e.className = "";
-    },5000);
-  }
-
   function load_open_options() {
     for (var key in localStorage){
       if(key.indexOf("_editables") > 0) {
@@ -249,5 +190,52 @@ window.onload = function() {
     a.addEventListener('click', function(e) {
       e.preventDefault();
       load_editable(this.getAttribute("data-name"));
+    });
+  }
+  function add_tab(name, options) {
+    if($('.tabs li[data-name='+name+']').length > 0) {
+      return false;
+    }
+
+    var add_close_link = options['close'] == true ? true : false;
+    var tab_content = options['content'] !== undefined ? options['content'] : localStorage.getItem(name + "_editables");
+
+    tabs[name] = {
+      'content': tab_content,
+      'edited': false
+    }
+    
+    var li=document.createElement("li");
+    li.className = "active";
+    li.setAttribute('data-name',name);
+
+    var a=document.createElement("a");      
+    a.setAttribute('data-name', name);
+    a.setAttribute('href', '#');
+    a.className = 'tab';
+    a.innerHTML = name;
+    li.appendChild(a);
+
+    if(add_close_link) {
+      var close_link = document.createElement('a');
+      close_link.setAttribute('data-name', name);
+      close_link.setAttribute('data-action','close');
+      close_link.setAttribute('href', '#');
+      close_link.innerHTML = " x"
+      close_link.addEventListener('click', function() {
+        close_tab_and_save(this.getAttribute('data-name'));
+      });
+      li.appendChild(close_link);      
+    }
+
+    $('#myTab li[data-action=new]').before(li);
+    a.addEventListener('dblclick', function() {
+      renaming = a.getAttribute('data-name');
+      $('#renameModal').modal('show');
+    });
+
+    a.addEventListener('click', function(e) {
+      e.preventDefault();
+      switch_tab(a);
     });
   }
